b4cff2be4a60bbc4f6c6e08815da60b2402fae6f
Isaac <isaachill13@gmail.com>
2015-09-29 13:08:42 +1000

    the coming of the beast..blade

---
+++ src/RidgyDidge.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+/**
+ * Created by Isaac-macPro on 29/09/15.
+ */
+public class RidgyDidge {
+    static void main(String arg[]) throws IOException {
+        
+    }
+}
