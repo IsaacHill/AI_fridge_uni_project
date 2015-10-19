28dfddfcc79488b15db19b47fe9f8315efee1778
Dominic Mulherin <undying-nightfall@hotmail.com>
2015-10-19 16:29:53 +1000

    Added the Link class, played with State

--- src/problem/Link.java
+++ src/problem/Link.java
@@ -1,8 +1,48 @@
 package problem;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Isaac-macPro on 19/10/15.
  */
 public class Link {
 
+    // Global Variables
+    private State fromState;
+    private List<Integer> action;
+    private int numDone;
+    private List<State> toStates;
+
+    public Link(State fromState, List<Integer> action) throws IOException {
+        if (fromState == null) throw new IOException("fromState was null");
+        if (action == null) throw new IOException("action was null");
+        try {
+            this.fromState = fromState;
+            this.action = cloneAction(action);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("fromState was " + fromState.getClass() + " instead of State");
+        }
+        numDone = 0;
+        toStates = new ArrayList<>();
+    }
+
+    public List<Integer> getAction() { return cloneAction(action); }
+
+    public void actionTaken() { numDone++; }
+
+    public int getTimesTaken() { return numDone; }
+
+    public List<State> getNextStates() { return toStates; }
+
+    public void addNextState(State s) { toStates.add(s); }
+
+    private List<Integer> cloneAction(List<Integer> action) {
+        List<Integer> newAction = new ArrayList<>();
+        for (int i : action) {
+            newAction.add(i);
+        }
+        return newAction;
+    }
 }
--- src/problem/State.java
+++ src/problem/State.java
@@ -8,29 +8,17 @@ import java.util.*;
  */
 public class State {
     private List<Integer> state;
-    private Double totalFail = 0.0;
     private int reward = 0;
     private ProblemSpec spec;
     private Map<List<Integer>,Double> actions = null;
     private int timesVisited;
+    private Double totalFail = 0.0;
+
 
     public State(List<Integer> state, ProblemSpec spec) throws IOException {
         this.state = state;
         this.spec = spec;
         // TODO: List of pseudo states available to go to
-
-    }
-
-    public void calculateFailure() {
-        for (int i = 0; i < state.size()-1; i++) {
-            int amount = state.get(i);
-            List<Double> probs = spec.getProbabilities().get(i).getRow(amount);
-            Double failure = 0.0;
-            for (int d = probs.size()-1; d > amount; d--) {
-                failure += probs.get(d)*(d-amount);
-            }
-            totalFail += failure;
-        }
 
     }
 
@@ -42,8 +30,21 @@ public class State {
         return reward;
     }
 
-    public void setReward(int reward) {
-        this.reward = reward;
-    }
+    public void setReward(int reward) { this.reward = reward; }
     //public Array
+
+    private void calculateFailure() {
+        for (int i = 0; i < state.size()-1; i++) {
+            int amount = state.get(i);
+            List<Double> probs = spec.getProbabilities().get(i).getRow(amount);
+            Double failure = 0.0;
+            for (int d = probs.size()-1; d > amount; d--) {
+                failure += probs.get(d)*(d-amount);
+            }
+            totalFail += failure;
+        }
+    }
+
+    public Double getTotalFailure() { return totalFail; }
+
 }
