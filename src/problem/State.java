276caca2983f6b0e2a8271e6a590c6cd2906ef34
Isaac <isaachill13@gmail.com>
2015-10-19 16:11:22 +1000

    added some serious shizz

---
+++ src/problem/State.java
@@ -0,0 +1,63 @@
+package problem;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Dom on 19/10/2015.
+ */
+public class State {
+    private List<Integer> state;
+    private List<Matrix> probabilities = null;
+    private Double totalFail = 0.0;
+    private int reward = 0;
+    private ProblemSpec spec;
+    private Map<List<Integer>,Double> actions = null;
+
+    public State(List<Integer> state, ProblemSpec spec) throws IOException {
+        this.state = state;
+        this.spec = spec;
+        this.probabilities = spec.getProbabilities();
+        // TODO: List of pseudo states available to go to
+
+    }
+
+    public void calculateFailure() {
+        for (int i = 0; i < state.size()-1; i++) {
+            int amount = state.get(i);
+            List<Double> probs = probabilities.get(i).getRow(amount);
+            Double failure = 0.0;
+            for (int d = probs.size()-1; d > amount; d--) {
+                failure += probs.get(d)*(d-amount);
+            }
+            totalFail += failure;
+        }
+
+    }
+
+    public List<Integer> getState() {
+        return state;
+    }
+
+    public int getReward() {
+        return reward;
+    }
+
+    public void setReward(int reward) {
+        this.reward = reward;
+    }
+
+    public HashMap<List<Integer>, Double> createActions() {
+        int everyType = spec.getFridge().getMaxTypes();
+        int maxType = spec.getFridge().getMaxItemsPerType();
+        List<Integer> current = getState();
+        for (Integer type : current) {
+
+        }
+
+
+    }
+    //public Array
+}
